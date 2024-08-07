55aa7e835ce1c591aa69af8696b02be331b14a17
VrushaliHaldankar01 <vrushalihaldankar01@gmail.com>
2024-08-06 20:58:00 -0400

    added search bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import VendorRegister from '../pages/VendorRegister';
 import CategoryPage from '../pages/CategoryPage';
-import Profile from '../pages/Profile';  // Import Profile component
+import Profile from '../pages/Profile'; // Import Profile component
 import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Terms from '../pages/Terms';
@@ -13,10 +13,13 @@ import Privacy from '../pages/Privacy';
 import UserRegister from '../pages/UserRegister';
 import Login from '../pages/Login';
 import VendorDashboard from '../pages/VendorDashboard';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <Router>
+      <ToastContainer />
+
       <Routes>
         <Route path='/VendorRegister' element={<VendorRegister />} />
         <Route path='/' element={<Dashboard />} />
@@ -24,12 +27,14 @@ function App() {
         <Route path='/UserRegister' element={<UserRegister />} />
         <Route path='/Login' element={<Login />} />
         <Route path='/categories' element={<CategoryPage />} />
-        <Route path='/Profile' element={<Profile />} />  {/* Add Profile route */}
+        <Route path='/Profile' element={<Profile />} />{' '}
+        {/* Add Profile route */}
         <Route path='/About' element={<About />} />
         <Route path='/Contact' element={<Contact />} />
         <Route path='/Terms' element={<Terms />} />
         <Route path='/Privacy' element={<Privacy />} />
         <Route path='/VendorDashboard' element={<VendorDashboard />} />
+        <Route path='/categories/:name' element={<CategoryPage />} />
       </Routes>
     </Router>
   );
